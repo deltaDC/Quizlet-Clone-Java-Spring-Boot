4e1b5c22e018d8ad48c6a181b7a69594aabacaca
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-01 16:35:20 +0700

    update user entity

--- src/main/java/com/deltadc/quizletclone/user/User.java
+++ src/main/java/com/deltadc/quizletclone/user/User.java
@@ -1,7 +1,9 @@
 package com.deltadc.quizletclone.user;
 
-
+import com.deltadc.quizletclone.folder.Folder;
+import com.deltadc.quizletclone.review.Review;
 import com.deltadc.quizletclone.set.Set;
+import com.deltadc.quizletclone.studysession.StudySession;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,18 +12,18 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data // sinh ra getter, setter, toString,... tu dong
-@Builder // tao doi tuong linh hoat hon (khong can dung vi tri tham so)
-@NoArgsConstructor // tao constructor ko doi tu dong
+@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
-@Entity // danh dau la entity trong database
-@Table(name = "user") // table voi ten la "user"
+@Entity
+@Table(name = "users")
 public class User {
 
-    // danh dau la id va auto_increment
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long user_id;
+    @Column(name = "user_id")
+    private Long userId;
 
     private String username;
 
@@ -31,5 +33,15 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Set> sets;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Folder> folders;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private StudySession studySession;
+
 
 }
