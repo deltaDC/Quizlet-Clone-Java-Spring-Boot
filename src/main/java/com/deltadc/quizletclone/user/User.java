d4707fafd51741f57228f7173bbe0e75715d9eae
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-01 16:37:53 +0700

    update user entity

--- src/main/java/com/deltadc/quizletclone/user/User.java
+++ src/main/java/com/deltadc/quizletclone/user/User.java
@@ -1,5 +1,6 @@
 package com.deltadc.quizletclone.user;
 
+
 import com.deltadc.quizletclone.folder.Folder;
 import com.deltadc.quizletclone.review.Review;
 import com.deltadc.quizletclone.set.Set;
@@ -12,18 +13,19 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
-@Builder
-@NoArgsConstructor
+@Data // sinh ra getter, setter, toString,... tu dong
+@Builder // tao doi tuong linh hoat hon (khong can dung vi tri tham so)
+@NoArgsConstructor // tao constructor ko doi tu dong
 @AllArgsConstructor
-@Entity
-@Table(name = "users")
+@Entity // danh dau la entity trong database
+@Table(name = "user") // table voi ten la "user"
 public class User {
 
+    // danh dau la id va auto_increment
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long userId;
+    private Long user_id;
 
     private String username;
 
@@ -42,6 +44,4 @@ public class User {
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private StudySession studySession;
-
-
 }
