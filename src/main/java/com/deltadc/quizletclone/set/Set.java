681e487d5a2b97053c6f6287393efe4879a43ff4
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-01 16:22:57 +0700

    define set entity

--- src/main/java/com/deltadc/quizletclone/set/Set.java
+++ src/main/java/com/deltadc/quizletclone/set/Set.java
@@ -1,4 +1,42 @@
 package com.deltadc.quizletclone.set;
 
+import com.deltadc.quizletclone.card.Card;
+import com.deltadc.quizletclone.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "set")
 public class Set {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "set_id")
+    private Long setId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+
+    private String description;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    private boolean isPublic;
+
+    @OneToMany(mappedBy = "set", cascade = CascadeType.ALL)
+    private List<Card> cards;
 }
