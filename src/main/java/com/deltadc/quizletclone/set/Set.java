89a80a4a6d33e4b48c297ff9907abbb3337836df
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-01 17:12:32 +0700

    update set entity

--- src/main/java/com/deltadc/quizletclone/set/Set.java
+++ src/main/java/com/deltadc/quizletclone/set/Set.java
@@ -1,6 +1,8 @@
 package com.deltadc.quizletclone.set;
 
 import com.deltadc.quizletclone.card.Card;
+import com.deltadc.quizletclone.folder.Folder;
+import com.deltadc.quizletclone.review.Review;
 import com.deltadc.quizletclone.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -21,7 +23,7 @@ public class Set {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "set_id")
-    private Long setId;
+    private Long set_id;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
@@ -39,4 +41,11 @@ public class Set {
 
     @OneToMany(mappedBy = "set", cascade = CascadeType.ALL)
     private List<Card> cards;
+
+    @ManyToMany(mappedBy = "set")
+    private List<Folder> folders;
+
+    @OneToMany(mappedBy = "set", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
 }
