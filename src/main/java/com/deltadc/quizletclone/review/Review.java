e0e79dde3824167c85213ebe8e8a4e26101f177f
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-02 15:26:43 +0700

    update review entity

--- src/main/java/com/deltadc/quizletclone/review/Review.java
+++ src/main/java/com/deltadc/quizletclone/review/Review.java
@@ -1,4 +1,35 @@
 package com.deltadc.quizletclone.review;
 
+import com.deltadc.quizletclone.set.Set;
+import com.deltadc.quizletclone.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "review")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long review_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "set_id", nullable = false, unique = true)
+    private Set set;
+
+    @Column(name = "total_stars", nullable = false)
+    private int totalStars;
+
 }
