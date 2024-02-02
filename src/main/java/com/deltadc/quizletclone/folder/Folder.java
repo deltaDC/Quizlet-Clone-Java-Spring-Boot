d78127ed9e2549c1696534b2dff5c03d2e49f92f
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-02 15:25:39 +0700

    update folder entity

--- src/main/java/com/deltadc/quizletclone/folder/Folder.java
+++ src/main/java/com/deltadc/quizletclone/folder/Folder.java
@@ -1,4 +1,42 @@
 package com.deltadc.quizletclone.folder;
 
+import com.deltadc.quizletclone.folderset.FolderSet;
+import com.deltadc.quizletclone.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "folder")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "folder_id")
+    private Long folder_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+
+    private String description;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    private boolean isPublic;
+
+    @OneToOne(mappedBy = "folder")
+    private List<FolderSet> folderSet;
 }
