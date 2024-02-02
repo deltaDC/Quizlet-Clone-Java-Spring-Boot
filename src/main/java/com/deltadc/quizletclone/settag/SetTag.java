d4ad45152dac597f9080cec295296c00b3a1dac8
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-02 15:27:34 +0700

    update settag entity

--- src/main/java/com/deltadc/quizletclone/settag/SetTag.java
+++ src/main/java/com/deltadc/quizletclone/settag/SetTag.java
@@ -1,4 +1,33 @@
 package com.deltadc.quizletclone.settag;
 
+import com.deltadc.quizletclone.set.Set;
+import com.deltadc.quizletclone.tag.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "set_tags")
 public class SetTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "set_tag_id")
+    private Long set_tag_id;
+
+    @OneToOne
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    @OneToOne
+    @JoinColumn(name = "set_id", nullable = false, unique = true)
+    private Set set;
+
+
 }
