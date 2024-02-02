1e6908ef3251fcd05278c098562d44de3e27a142
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-02 15:28:02 +0700

    update tag entity

--- src/main/java/com/deltadc/quizletclone/tag/Tag.java
+++ src/main/java/com/deltadc/quizletclone/tag/Tag.java
@@ -1,4 +1,23 @@
 package com.deltadc.quizletclone.tag;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tag")
 public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long tag_id;
+
+    private String name;
 }
