4695009f4c173b522938d73126919e392338cfaf
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-04 15:27:02 +0700

    add signup new user api

--- src/main/java/com/deltadc/quizletclone/user/UserController.java
+++ src/main/java/com/deltadc/quizletclone/user/UserController.java
@@ -2,10 +2,9 @@ package com.deltadc.quizletclone.user;
 
 import com.deltadc.quizletclone.set.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +14,26 @@ public class UserController {
 
     private final UserService userService;
 
+
+    // dang ky user moi
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody User user) {
+        System.out.println("Dữ liệu nhận được từ phía client:");
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Email: " + user.getEmail());
+
+        // Thực hiện logic đăng ký người dùng trong service
+        boolean signUpSuccess = userService.signUp(user);
+
+        if (signUpSuccess) {
+            return ResponseEntity.ok("Đăng ký thành công");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Đăng ký thất bại");
+        }
+    }
+
+
+    // lay tat ca cac set cua 1 user
     @GetMapping(path = "/{username}/sets")
     public ResponseEntity<List<Set>> getProfile(@PathVariable("username") String username) {
         List<Set> userSets = userService.getUserSets(username);
--- src/main/java/com/deltadc/quizletclone/user/UserService.java
+++ src/main/java/com/deltadc/quizletclone/user/UserService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +13,22 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public boolean signUp(User user) {
+        // Kiểm tra xem người dùng có tồn tại dựa trên email hay không
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+
+        if (existingUser.isPresent()) {
+            return false;
+        }
+
+        // Người dùng chưa tồn tại, thêm mới
+        userRepository.save(user);
+        return true;
+    }
+
     public List<Set> getUserSets(String username) {
         return null;
     }
+
+
 }
