9588e10b0e6dfc5dd82f965a61be36a2565b68e7
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-07 13:55:23 +0700

    add getName() method
    because getUsername() is override from UserDetails interface by spring, use getName() to send username to frontend

--- src/main/java/com/deltadc/quizletclone/user/User.java
+++ src/main/java/com/deltadc/quizletclone/user/User.java
@@ -52,6 +52,10 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public String getName() {
+        return username;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
