ab5a179c69627046aa460f6ea737caec8cd0343b
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2024-02-17 21:13:11 +0700

    add findByUserId

--- src/main/java/com/deltadc/quizletclone/set/SetRepository.java
+++ src/main/java/com/deltadc/quizletclone/set/SetRepository.java
@@ -1,6 +1,12 @@
 package com.deltadc.quizletclone.set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SetRepository extends JpaRepository<Set, Long> {
+    @Query("SELECT s FROM Set s WHERE s.user.id = :userId")
+    List<Set> findByUserId(@Param("userId") Long userId);
 }
