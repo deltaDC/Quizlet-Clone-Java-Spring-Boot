220e303aaf8d53b04bb3af17df7acfac98e35269
deltaDC <ducchinh.work@gmail.com>
2024-04-23 09:46:49 +0700

    add validate login signup

--- src/main/java/com/deltadc/quizletclone/auth/AuthenticationController.java
+++ src/main/java/com/deltadc/quizletclone/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.deltadc.quizletclone.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +15,31 @@ public class AuthenticationController {
 
     @PostMapping("/signup")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody SignUpRequest request) {
+        if(request.getUsername().isEmpty() || request.getEmail().isEmpty() || request.getPassword().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(
+                            AuthenticationResponse.builder()
+                                    .message("khong duoc de trong")
+                                    .build()
+                    );
+        }
+
         System.out.println(request.getUsername() + " " + request.getPassword() + " " + request.getPassword());
         return authenticationService.signup(request);
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        if(request.getEmail().isEmpty() || request.getPassword().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(
+                            AuthenticationResponse.builder()
+                                    .message("khong duoc de trong")
+                                    .build()
+                    );
+        }
+
+
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 }
