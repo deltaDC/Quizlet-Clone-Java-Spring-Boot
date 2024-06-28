97af41a43668d1bd6355c91ad8e39f965f431444
deltadc <ducchinh.work@gmail.com>
2024-06-28 16:38:00 +0700

    refactor and clean TagService code

--- src/main/java/com/deltadc/quizletclone/tag/TagService.java
+++ src/main/java/com/deltadc/quizletclone/tag/TagService.java
@@ -10,40 +10,30 @@ import java.util.List;
 public class TagService {
     private final TagRepository tagRepository;
 
-    // Lấy ra tất cả tag
     public List<Tag> getAllTags() {
         return tagRepository.findAll();
     }
 
-    // Lấy ra 1 tag
     public Tag getTag(Long tag_id) {
-        return tagRepository.findById(tag_id).orElse(null);
+        return tagRepository.findById(tag_id).orElseThrow();
     }
 
     public Tag createTag(Tag newTag) {
-        Tag tag = new Tag();
-        tag.setName(newTag.getName());
-        tagRepository.save(newTag);
-        return tag;
+        Tag tag = Tag.builder()
+                .name(newTag.getName())
+                .build();
+        return tagRepository.save(tag);
     }
 
-    // Update 1 tag
     public Tag updateTag(Long tag_id, Tag newTag) {
-        Tag tag = tagRepository.findById(tag_id).orElse(null);
-        if (tag == null) {
-            return null;
-        }
+        Tag tag = tagRepository.findById(tag_id).orElseThrow();
         tag.setName(newTag.getName());
         tagRepository.save(tag);
         return tag;
     }
 
-    // Xóa 1 tag
     public String deleteTag(Long tag_id) {
-        Tag tag = tagRepository.findById(tag_id).orElse(null);
-        if (tag == null) {
-            return "Cannot find this tag!";
-        }
+        tagRepository.findById(tag_id).orElseThrow(null);
         tagRepository.deleteById(tag_id);
         return "Deleted!";
     }
